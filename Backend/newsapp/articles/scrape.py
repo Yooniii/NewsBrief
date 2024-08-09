01e651c0a158aab7c23069699277aedeba0ceb11
Sarah Yoon <sarahyoon705@gmail.com>
2024-08-09 00:50:07 -0400

    Removed unnecessary code

--- Backend/newsapp/articles/scrape.py
+++ Backend/newsapp/articles/scrape.py
@@ -1,29 +1,17 @@
 import newspaper
 
-def get_authors(page):
-  return page.authors
-
-def get_content(page):
-  return page.text.replace('\n', '')
-
-def get_image(page):
-  return page.top_image
-
-def get_date(page):
-  return page.publish_date
-
 def scrape(url, method):
   page = newspaper.article(url)
   page.download()
   page.parse()
   
   if method == 'author':
-    get_authors(page)
+    return page.authors
   elif method == 'image':
-    get_image(page)
+    return page.top_image
   elif method == 'date':
-    get_date(page)
+    return page.publish_date
   elif method == 'content':
-    get_content(page)
+    return page.text.replace('\n', '')
   
 
