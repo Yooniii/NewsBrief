c0c5a7620bf50dbbe9ff6e10cdb31e52f2556395
Sarah Yoon <sarahyoon705@gmail.com>
2024-08-08 21:41:32 -0400

    Added multithreading

--- Backend/newsapp/articles/tasks.py
+++ Backend/newsapp/articles/tasks.py
@@ -1,51 +1,105 @@
-import feedparser
 from articles.models import Article
 from .decode_url import decode_google_news_url
 from .scrape import scrape
+from newscatcherapi import NewsCatcherApiClient
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import feedparser
+import requests
+import os
+import concurrent.futures
+
 
 class BackgroundClass:
 
   @staticmethod
   def fetch_articles(category, url):
-    feed = feedparser.parse(url)
-    count = 0
+    print(f"Fetching articles from URL: {url}")
 
-    for entry in feed.entries:
-      if count >= 10:
-        break
+    if 'news.google' in url:
+      count = 0
+      feed = feedparser.parse(url)
 
-      try: 
-        decoded_url = decode_google_news_url(entry.link)
-        date, image, content, summary = scrape(decoded_url)
+      for entry in feed.entries:
+        if count >= 10:
+          break
+
+        try: 
+          decoded_url = decode_google_news_url(entry.link)
+          date, image, content, summary = scrape(decoded_url)
+
+          Article.objects.create(
+            title=entry.title.split(' - ')[0],
+            date=date,
+            source=entry.source.title,
+            article_link=decoded_url,
+            img_url=image,
+            content=content,
+            summary=summary,
+            category=category 
+          )
+          count+=1
+          print('Successfully added new article')    
         
-        Article.objects.create(
-          title=entry.title.split(' - ')[0],
-          date=date,
-          source=entry.source.title,
-          article_link=decoded_url,
-          img_url=image,
-          content=content,
-          summary=summary,
-          category=category 
-        )
-        count+=1
-        print('Successfully added new article')
-      
-      except Exception as e:
-        print(f'error{e}')
-      
-    count = 0
+        except Exception as e:
+          print(f'error{e}')
+            
+      count = 0
 
+    else:
+      headers = {
+        'x-api-key': os.getenv('NEWS_API_KEY')
+      }
+
+      response = requests.get(url, headers=headers)
+      data = response.json()
+      articles = data.get('articles', [])
+
+      for article in articles:
+        try:
+          date, image, content, summary = scrape(article.get('link'))
+
+          Article.objects.create(
+            title=article.get('title'),
+            date=article.get('published_date'),
+            source=article.get('author'),
+            article_link=article.get('link'),
+            img_url=article.get('media'),
+            content=content,
+            summary=summary,
+            category=category,
+          )
+          print('Successfully added new article')
+        
+        except Exception as e:
+          print(f'error{e}')
+    
+    
   @staticmethod
   def upload_data():
-    urls = {'Top Stories': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Business': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx6TVdZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Tech': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Entertainment': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNREpxYW5RU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Sports': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Science': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-              'Health': 'https://news.google.com/rss/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JXVnVMVWRDS0FBUAE?hl=en-CA&gl=CA&ceid=CA%3Aen'
-            }
-        
-    for category, url in urls.items():
-      BackgroundClass.fetch_articles(category, url)
+    urls = {
+      # 'Top Stories': 'https://api.newscatcherapi.com/v2/latest_headlines?countries=CA,US&not_sources=oilersnation.com',
+      'Top Stories': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Business': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx6TVdZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Tech': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Entertainment': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNREpxYW5RU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Politics': 'https://api.newscatcherapi.com/v2/latest_headlines?countries=CA,US&not_sources=oilersnation.com&topic=politics&page_size=10',
+      'Sports': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Science': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
+      'Health': 'https://news.google.com/rss/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JXVnVMVWRDS0FBUAE?hl=en-CA&gl=CA&ceid=CA%3Aen'
+    }
+    
+    with ThreadPoolExecutor(max_workers=8) as executor:
+      future_to_article = {
+        executor.submit(BackgroundClass.fetch_articles, category, url): 
+        category for category, url in urls.items()
+      }
+
+      for future in concurrent.futures.as_completed(future_to_article):
+        article = future_to_article[future]
+        try:
+          future.result()
+        except Exception as e:
+          print(f'Failed to upload data for {article} {e}')
+
