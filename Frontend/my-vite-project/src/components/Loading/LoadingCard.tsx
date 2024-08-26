19e8c4878beecee7487eb17f7d691e34ed66db93
Sarah Yoon <sarahyoon705@gmail.com>
2024-08-26 14:20:02 -0400

    Fixed loading card

--- Frontend/my-vite-project/src/components/Loading/LoadingCard.tsx
+++ Frontend/my-vite-project/src/components/Loading/LoadingCard.tsx
@@ -6,12 +6,12 @@ const LoadingCard = () => {
     <Fragment>
       <div className='load-card'>
         <div className='img-container'>
-          <CustomSkeleton width={350} height={252} variant="rounded"/>
+          <CustomSkeleton width={380} height={252} variant="rounded"/>
         </div>
           <div className="text">
-            <CustomSkeleton width={670} height={120} variant="rounded"/>
-            <CustomSkeleton width={670} height={24} variant="rounded"/>
-            <CustomSkeleton width={670} height={72} variant="rounded"/>
+            <CustomSkeleton width={800} height={120} variant="rounded"/>
+            <CustomSkeleton width={800} height={24} variant="rounded"/>
+            <CustomSkeleton width={800} height={72} variant="rounded"/>
           </div>
       </div>
     </Fragment>
