f4b261e17b5bcb113f244286ea4fdc411ef4ad4a
Sarah Yoon <sarahyoon705@gmail.com>
2024-08-28 02:15:33 -0400

    Cleaned code

--- Backend/newsapp/articles/scrape.py
+++ Backend/newsapp/articles/scrape.py
@@ -1,17 +1,17 @@
 import newspaper
 
-def scrape(url, method):
+def scrape(url):
+  """
+  Scrapes the image, date, raw text, and possibly videos from the given news url
+
+  Args:
+    url: the news article URL
+    
+  """
   page = newspaper.article(url)
   page.download()
   page.parse()
   
-  if method == 'top_image':
-    return page.top_image
-  elif method == 'date':
-    return page.publish_date
-  elif method == 'content':
-    return page.text.replace('\n', '')
-  elif method =='media':
-    return page.movies
+  return page.publish_date, page.top_image, page.text.replace('\n', ''), page.movies
   
 
