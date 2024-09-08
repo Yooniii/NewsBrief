d503cd3b4bb5455c4e5c98992e09b5c8e64ef329
Sarah Yoon <sarahyoon705@gmail.com>
2024-09-08 18:19:34 -0400

    Fixed RSS Feedparser

--- Backend/newsapp/articles/tasks.py
+++ Backend/newsapp/articles/tasks.py
@@ -1,11 +1,13 @@
 from articles.models import Article
-from rest_framework.decorators import api_view
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from .decode_url import decode_google_news_url
 from .scrape import scrape
 from .summarize import summarize
+from urllib.request import ProxyHandler
 import feedparser
-import concurrent.futures
+from urllib.request import ProxyHandler
+import json
+import os
 
 class BackgroundClass:
   """
@@ -29,24 +31,22 @@ class BackgroundClass:
     print(f"Fetching articles from URL: {url}")
 
     count = 0
+
     feed = feedparser.parse(url)
 
     for entry in feed.entries:
-
-      # Fetch only ten articles from the given Feed at a time
-      if count >= 10:
+      # Fetch only ten articles from the Feed at a time
+      if count >= 3:
         break
       
-      print(entry.link)
-
       try: 
         
         # Scrape the article content and summarize it
-        decoded_url = decode_google_news_url(entry.link) # Get the original link
-        print(f"Decoded URL: {decoded_url}")  # Add this line
+        # decoded_url = decode_google_news_url(entry.link) 
+        # print(f"DECODED URL: {decoded_url}")  
         
-        date, top_image, content, media = scrape(decoded_url)
-        title = entry.title.split(' - ')[0]
+        source, date, top_image, content, media = scrape(entry.link)
+        title = entry.title
         summary = summarize(content, title)
         
         # If the summary is valid, save the article to the database
@@ -54,8 +54,8 @@ class BackgroundClass:
           Article.objects.create(
             title=title,
             date=date,
-            source=entry.source.title,
-            article_link=decoded_url,
+            source=source,
+            article_link=entry.link,
             top_image=top_image, 
             media=media,
             content=content,
@@ -67,9 +67,8 @@ class BackgroundClass:
           print('Successfully added new article')    
 
       except Exception as e:
-        print(f'error{e}')
-      
-            
+        print(f'ERROR: error{e}')
+                  
     count = 0
 
   @staticmethod
@@ -82,29 +81,16 @@ class BackgroundClass:
         None
     """
 
-    urls = {
-      'Top Stories': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFZxYUdjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'World': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx1YlY4U0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Business': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGx6TVdZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Tech': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRGRqTVhZU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Entertainment': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNREpxYW5RU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Politics': 'https://news.google.com/rss/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNRFZ4ZERBU0JXVnVMVWRDS0FBUAE?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Sports': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Science': 'https://news.google.com/rss/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp0Y1RjU0JXVnVMVWRDR2dKRFFTZ0FQAQ?hl=en-CA&gl=CA&ceid=CA%3Aen',
-      'Health': 'https://news.google.com/rss/topics/CAAqJQgKIh9DQkFTRVFvSUwyMHZNR3QwTlRFU0JXVnVMVWRDS0FBUAE?hl=en-CA&gl=CA&ceid=CA%3Aen'
-    }
+    current_directory = os.path.dirname(__file__)
+    file_path = os.path.join(current_directory, 'urls.json')
+
+    with open(file_path, 'r') as file:
+      urls = json.load(file)
+
     
     # Use ThreadPoolExecutor to fetch news articles from 7 categories at a time
     with ThreadPoolExecutor(max_workers=5) as executor:
-      future_to_article = {
-        executor.submit(BackgroundClass.fetch_articles, category, url): 
-        category for category, url in urls.items()
-      }
-
-      for future in concurrent.futures.as_completed(future_to_article):
-        article = future_to_article[future]
-        try:
-          future.result()
-        except Exception as e:
-          print(f'Failed to upload {article} {e}')
+      for category, url_arr in urls.items():
+        for url in url_arr:
+          executor.submit(BackgroundClass.fetch_articles, category, url)
 
