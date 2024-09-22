b677bcb073e1eca911c2fc104a25b9f5d498fd02
Sarah Yoon <sarahyoon705@gmail.com>
2024-09-22 15:01:00 -0400

    Update ArticleList.tsx

--- Frontend/my-vite-project/src/components/ArticleList/ArticleList.tsx
+++ Frontend/my-vite-project/src/components/ArticleList/ArticleList.tsx
@@ -21,7 +21,7 @@ const ArticleList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000/articles/');
+        const response = await axios.get('http://127.0.0.1:8000/articles/');                                                                                                                          
         const allArticles = response.data;
 
         let filteredArticles = allArticles;
