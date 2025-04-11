4c07b015151bf2c4c065879e04f5259015072829
Sarah Yoon <sarahyoon705@gmail.com>
2025-04-11 15:20:28 -0400

    Update jobs.py

--- Backend/newsapp/articles/jobs.py
+++ Backend/newsapp/articles/jobs.py
@@ -3,6 +3,6 @@ from .tasks import BackgroundClass
 
 def start():
   scheduler = BackgroundScheduler()
-  scheduler.add_job(BackgroundClass.upload_data, 'interval', minutes=3)
+  scheduler.add_job(BackgroundClass.upload_data, 'interval', minutes=1, max_instances=3)
   scheduler.start()
   
