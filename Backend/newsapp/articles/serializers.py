f182ce7330b88feef0c953353cf94fe294416695
Sarah Yoon <sarahyoon705@gmail.com>
2025-04-16 12:20:26 -0400

    Update serializers.py

--- Backend/newsapp/articles/serializers.py
+++ Backend/newsapp/articles/serializers.py
@@ -4,4 +4,4 @@ from articles.models import Article
 class ArticleSerializer(serializers.ModelSerializer):
   class Meta:
     model = Article
-    fields = '__all__'
+    fields = '__all__' # include all fields
